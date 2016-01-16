71e2be7b5a3143a506bf3d0b501716c13313a526
Marco Gutierrez <mgutierrez-nicolino@ucmerced.edu>
2016-01-15 22:00:17 -0800

    Added music-task and tested

--- source/app.js
+++ source/app.js
@@ -6,15 +6,30 @@ var bodyParser = require('body-parser');
 var logger = require('gruew-logger');
 var config = require('./config');
 var hello = require('./hello-world');
-
+var music_task = require('./music-task');
 
 function App() {
     this.run = function () {
-
         if (process.argv.length > 2 && process.argv[2] === 'hello') {
             hello();
-        } else if (process.argv.length > 2 && process.argv[2] === 'argument') {
-            // some other function
+        } else if (process.argv.length > 2 && process.argv[2] === 'music') {
+            var test = {
+   "userName":"andre@something.com",
+   "music": {
+       "Childish Gambino":[
+               "song1",
+               "song2",
+               "song3"
+           ],
+       "Taylor Swift":[
+               "song1",
+               "song2",
+               "song3"
+           ]
+   }
+};
+        music_task(test);
+        //some function that takes test and saves it to file/music.json
         } else {
             var app = express();
             app.use(bodyParser.json());
---
+++ source/music-task.js
@@ -0,0 +1,40 @@
+var jsonfile = require("jsonfile");
+var path = require('path');
+
+module.exports = function(test) {
+	// Grab a username and save it. Check the current file for the username 
+    console.log('Evening peeps, it is time for music!');
+var filePath = path.join(__dirname, 'Files/music.json');
+
+jsonfile.readFile(filePath, function(error, data){
+		if (error){
+			console.log(error);
+			return; 
+		}
+		console.log(data);
+		var user = test.userName;
+		var music = test.music;
+		console.log(user);
+		console.log(music);
+		data[user] = music ;
+
+	jsonfile.writeFile(filePath, data, function(error){
+		if (error){
+			console.log(error);
+			return;
+		}
+		console.log("We livin?");
+
+	});
+});
+
+
+
+
+
+
+console.log(test.userName);
+
+
+
+};
